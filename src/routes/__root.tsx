445a5d6b70bd4d2d128a744c756f2536fe045755
Selora Health <selorahealth@gmail.com>
2026-08-08 01:17:22 +0100

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -97,7 +97,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
+      { rel: "icon", href: "/favicon.io", type: "image/svg+xml" },
     ],
   }),
   shellComponent: RootShell,
